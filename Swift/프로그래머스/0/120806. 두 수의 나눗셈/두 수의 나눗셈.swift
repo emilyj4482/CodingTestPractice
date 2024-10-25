762dca505c06b87b9293ddffaf0875db1352d942
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-10-25 11:55:29 +0900

    Update 두 수의 나눗셈.swift

--- Swift/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
+++ Swift/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
@@ -3,3 +3,9 @@ import Foundation
 func solution(_ num1:Int, _ num2:Int) -> Int {
     return Int(Double(num1) / Double(num2) * 1000)
 }
+
+// other solution 1
+// 미리 1000을 곱하면 형변환 할 필요가 없다.
+func solution1(_ num1:Int, _ num2:Int) -> Int {
+    return num1 * 1000 / num2
+}
