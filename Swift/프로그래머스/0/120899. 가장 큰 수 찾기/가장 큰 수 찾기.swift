9b4872fc465665ff9798800fb292d490c6a5ae84
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-04 01:27:04 +0900

    Update 가장 큰 수 찾기.swift

--- Swift/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.swift
+++ Swift/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.swift
@@ -7,3 +7,9 @@ func solution(_ array:[Int]) -> [Int] {
     else { return [] }
     return [max, index]
 }
+
+// other code 1
+func solution1(_ array:[Int]) -> [Int] {
+    guard let max = array.enumerated().max(by: { $0.element < $1.element }) else { return [] }
+    return [max.element, max.offset]
+}
