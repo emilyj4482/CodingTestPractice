08f17cc198863117839dbedefe5d8eb3c74f8338
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-04 09:41:54 +0900

    Update x만큼 간격이 있는 n개의 숫자.swift

--- Swift/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.swift
+++ Swift/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.swift
@@ -1,3 +1,15 @@
+// my code : 0.10 ms
 func solution(_ x:Int, _ n:Int) -> [Int64] {
+    var result = [Int64]()
+    
+    for i in 1...n {
+        result.append(Int64(x * i))
+    }
+    
+    return result
+}
+
+// other code 1 : 0.65 ms
+func solution1(_ x:Int, _ n:Int) -> [Int64] {
     return Array(1...n).map { Int64($0 * x) }
 }
