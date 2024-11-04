7738dc68e521d2cec54e7fa96d5e4bf2d9e9b9ac
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-04 09:15:27 +0900

    Update 나머지가 1이 되는 수 찾기.swift

--- Swift/프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
+++ Swift/프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
@@ -1,8 +1,15 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    var answer = 1
+// my code : 시간 매우 오래 걸림
+func solution(_ n:Int) -> Int {    
+    guard let result = (1...n).filter { n % $0 == 1 }.min() else { return 2 }
 
+    return result
+}
+
+// other code 1 : 시간 훨씬 빠름
+func solution1(_ n: Int) -> Int {
+    var answer = 1
     while n % answer != 1 {
         answer += 1
     }
