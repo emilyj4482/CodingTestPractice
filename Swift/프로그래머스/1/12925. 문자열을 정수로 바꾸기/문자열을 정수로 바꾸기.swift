1e96a5f2a0d960a8142d2154c75028d051d9c4ac
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-05 09:24:24 +0900

    Update 문자열을 정수로 바꾸기.swift

--- Swift/프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
+++ Swift/프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
@@ -1,3 +1,8 @@
-func solution(_ s:String) -> Int {
+func solution(_s: String) -> Int {
+    guard let result = Int(s) else { return 0 }
+    return result
+}
+
+func solution1(_ s:String) -> Int {
     return Int(s) ?? 0
 }
