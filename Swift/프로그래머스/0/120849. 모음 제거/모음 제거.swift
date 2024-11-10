4095887e01617df0fbd5b9e4fea86e9d77932e40
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-11 00:27:45 +0900

    Update 모음 제거.swift

--- Swift/프로그래머스/0/120849. 모음 제거/모음 제거.swift
+++ Swift/프로그래머스/0/120849. 모음 제거/모음 제거.swift
@@ -5,3 +5,18 @@ func solution(_ my_string:String) -> String {
     let vowels = ["a", "e", "i", "o", "u"]
     return toArray.filter { !vowels.contains($0) }.joined()
 }
+
+// other code 1
+func solution1(_ my_string:String) -> String {
+    return my_string.filter { !["a", "e", "i", "o", "u"].contains($0)}
+}
+
+// other code 2
+func solution2(_ my_string:String) -> String {
+    return my_string.components(separatedBy: ["a", "e", "i", "o", "u"]).joined()
+}
+
+// other code 3
+func solution3(_ my_string:String) -> String {
+    return my_string.replacingOccurrences(of: "[aeiou]", with: "", options: .regularExpression)
+}
