ca8f3fe0c277cd7368a4464adfb096d4bd3a3bdb
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-13 09:18:33 +0900

    Update 음양 더하기.swift

--- Swift/프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ Swift/프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -9,3 +9,17 @@ func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     
     return array.reduce(0, +)
 }
+
+// other code 1
+func solution1(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    zip(absolutes, signs)
+        .map { $1 ? $0 : -$0 }
+        .reduce(0, +)
+}
+
+// other code2
+func solution2(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    return (0..<absolutes.count)
+        .map { signs[$0] ? absolutes[$0] : -absolutes[$0] }
+        .reduce(0, +)
+}
