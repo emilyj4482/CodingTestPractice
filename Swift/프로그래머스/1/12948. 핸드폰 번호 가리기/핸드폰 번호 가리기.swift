e5dffeb87f310daf304885f874af34388c0b3104
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-14 09:28:06 +0900

    Update 핸드폰 번호 가리기.swift

--- Swift/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
+++ Swift/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.swift
@@ -7,3 +7,8 @@ func solution(_ phone_number: String) -> String {
     
     return result
 }
+
+// other code 1
+func solution1(_ phone_number:String) -> String {
+    return String(repeating: "*", count: phone_number.count - 4) + phone_number.suffix(4)
+}
