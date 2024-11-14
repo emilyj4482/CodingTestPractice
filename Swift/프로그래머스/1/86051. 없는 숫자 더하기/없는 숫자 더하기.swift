b8f3efedc7a68227a27ccb3a2e4157d024b7adc7
Emily <68208814+emilyj4482@users.noreply.github.com>
2024-11-14 09:33:30 +0900

    Update 없는 숫자 더하기.swift

--- Swift/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
+++ Swift/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -3,6 +3,10 @@ import Foundation
 func solution(_ numbers:[Int]) -> Int {
     var filtered = (0...9).filter { !numbers.contains($0) }
     
-    
     return filtered.reduce(0, +)
+}
+
+// other code 1
+func solution1(_ numbers: [Int]) -> Int {
+    return (0...9).filter { !numbers.contains($0) }.reduce(0, +)
 }
